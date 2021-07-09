d709eb6076fbcb8e721a1c17aa0ab5094455923c
Markin Maksim <37059479+MarkinMaksim@users.noreply.github.com>
2021-07-09 21:14:16 +0300

    Done (#12)
    Co-authored-by: MaksimMarkin <maksim-markin@mail.ru>

---
+++ GOF Cutting shape/Task (Facade)/Task/Facade.Task1/OrderPlacement/PaymentSystemService.cs
@@ -0,0 +1,10 @@
+﻿namespace Facade.Task1.OrderPlacement
+{
+    public class PaymentSystemService : PaymentSystem
+    {
+        public bool MakePayment(Payment payment)
+        {
+            return true;
+        }
+    }
+}
